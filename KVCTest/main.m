35be0dd784b473b533a8b21d7cab3a8db05d856c
yume190 <yume190@gmail.com>
2014-07-26 07:08:57 +0800

    Initial Commit

---
+++ KVCTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  KVCTest
+//
+//  Created by Yume on 2014/7/26.
+//  Copyright (c) 2014年 Yume. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
